fc6ce611465b51c36ce0451bd2dbebbd0fdfda9f
shitang <tiffanyshi.tang@mail.utoronto.ca>
2021-11-22 22:33:10 -0500

    overall example layout

--- examples.js
+++ examples.js
@@ -1,2 +1,20 @@
-const gallery = new GalleryGenerator([], null);
+const gallery = new GalleryGenerator([
+    {name: "photo1", path: 'photos/a.jpg', caption: 'Los Angeles', date: 'December 2018'},
+    {name: "photo2", path: 'photos/a1.jpg', caption: 'Sunset', date: 'December 2018'},
+    {name: "photo3", path: 'photos/a2.jpg', caption: 'San Francisco', date: 'December 2018'},
+    {name: "photo4", path: 'photos/a3.jpg', caption: 'San Francisco', date: 'December 2018'}], null);
 gallery.makeGallery();
+gallery.showCaption();
+gallery.showDate();
+
+function templateMosaic() {
+    gallery.templateMosaic();
+}
+
+function templateGrid() {
+    gallery.templateGrid();
+}
+
+function templateMansonry() {
+    gallery.templateMansonry();
+}
--- sandboxGallery.js
+++ sandboxGallery.js
@@ -15,16 +15,49 @@ GalleryGenerator.prototype = {
 
     makeGallery: function() {
         const gallery = document.createElement('div');
-        gallery.id = "sandboxgallery";
+        gallery.id = 'sandboxgallery';
         gallery.style.cssText = 'width:40px; height: 40px; background-color: Aqua;';
-        
+
         const body = $('body');
 		body.append(gallery);
+        this.setImage();
     },
 
     // parameter = array of image?
     setImage: function() {
+        const gallery = document.getElementById('sandboxgallery');
+        this.photos.map(photo => {
+            const div = document.createElement('div');
+            const img = document.createElement('img');
+            div.id = photo.name;
+            img.src = photo.path;
+            div.style.cssText = 'width: 350px;'
+            img.style.cssText = 'height: 350px';
+            gallery.appendChild(div);
+            div.append(img);
+        });
+    },
 
+    showCaption: function() {
+        this.photos.map(photo => {
+            const img_div = document.getElementById(photo.name);
+            const caption = document.createElement('span');
+            caption.style.cssText = 'display: block; color: blue; width: 100%;'
+            caption.innerHTML = photo.caption;
+            img_div.append(caption);
+            console.log(photo.caption);
+        });
+    },
+
+    showDate: function() {
+        this.photos.map(photo => {
+            const img_div = document.getElementById(photo.name);
+            const date = document.createElement('span');
+            date.style.cssText = 'display: block; color: red; width: 100%;'
+            date.innerHTML = photo.date;
+            img_div.append(date);
+            console.log(photo.date);
+        });
     },
     
     // parameter = image: (imageName, imagePath, caption)
@@ -54,8 +87,15 @@ GalleryGenerator.prototype = {
         }
     },
 
-    // Set style to template 1
-    template1: function() {
+    templateMosaic: function() {
+        console.log('template mosaic');
+    },
 
+    templateGrid: function() {
+        console.log('template grid');
+    },
+
+    templateMansonry: function() {
+        console.log('template mansonry');
     }
 }
