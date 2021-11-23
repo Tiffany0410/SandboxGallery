dc31b1679a3e8eeff9d4e887b88284e6e0f28196
shitang <tiffanyshi.tang@mail.utoronto.ca>
2021-11-23 02:43:16 -0500

    fixed bugs

--- examples.js
+++ examples.js
@@ -1,28 +1,42 @@
 photoList = [
-    {name: "photo1", path: 'photos/a.jpg', caption: 'Los Angeles', date: 'December 2018'},
-    {name: "photo2", path: 'photos/a1.jpg', caption: 'Sunset and Tree', date: 'December 2018'},
-    {name: "photo3", path: 'photos/a2.jpg', caption: 'Lantern Man', date: 'December 2018'},
-    {name: "photo4", path: 'photos/a3.jpg', caption: 'San Francisco', date: 'December 2018'},
-    {name: "photo5", path: 'photos/a4.jpg', caption: 'San Diego', date: 'January 2019'},
+    {name: "photo", path: 'photos/a.jpg', caption: 'Los Angeles', date: 'December 2018'},
+    {name: "photo1", path: 'photos/a1.jpg', caption: 'Sunset and Tree', date: 'December 2018'},
+    {name: "photo2", path: 'photos/a2.jpg', caption: 'Lantern Man', date: 'December 2018'},
+    {name: "photo3", path: 'photos/a3.jpg', caption: 'San Francisco', date: 'December 2018'},
+    {name: "photo4", path: 'photos/a4.jpg', caption: 'San Diego', date: 'January 2019'},
+    {name: "photo5", path: 'photos/a5.jpg', caption: 'Death Valley', date: 'January 2019'},
+    {name: "photo6", path: 'photos/a6.jpg', caption: 'Death Valley', date: 'January 2019'},
+    {name: "photo7", path: 'photos/a7.jpg', caption: 'Death Valley', date: 'January 2019'},
+    {name: "photo8", path: 'photos/a8.jpg', caption: 'Death Valley', date: 'January 2019'},
+    {name: "photo9", path: 'photos/a9.jpg', caption: 'Death Valley', date: 'January 2019'},
+    {name: "photo10", path: 'photos/a10.jpg', caption: 'Death Valley', date: 'January 2019'},
+    {name: "photo11", path: 'photos/a11.jpg', caption: 'Death Valley', date: 'January 2019'},
+    {name: "photo12", path: 'photos/a12.jpg', caption: 'Black & White Film', date: 'January 2019'},
+    {name: "photo13", path: 'photos/a13.jpg', caption: 'Black & White Film', date: 'January 2019'},
+    {name: "photo14", path: 'photos/a14.jpg', caption: 'Black & White Film', date: 'January 2019'},
+    {name: "photo15", path: 'photos/a15.jpg', caption: 'Black & White Film', date: 'January 2019'},
 ]
 
 const gallery = new GalleryGenerator('default', true, true);
-gallery.makeGallery(photoList.slice(0, 4));
+gallery.makeGallery(photoList.slice(0, 16));
 
 function templateDefault() {
-    gallery.makeGallery(photoList.slice(0, 4));
+    // gallery.makeGallery(photoList.slice(0, 16));
     gallery.setTemplate('default');
+    gallery.makeGallery(photoList.slice(0 ,16));
 }
 
 function templateMosaic() {
-    gallery.setImage(photoList.slice(0,5));
     gallery.setTemplate('mosaic');
+    gallery.makeGallery(photoList.slice(5,12));
 }
 
 function templateGrid() {
     gallery.setTemplate('grid');
+    gallery.makeGallery(photoList.slice(0 ,4));
 }
 
 function templateMansonry() {
     gallery.setTemplate('mansonry');
+    gallery.makeGallery(photoList.slice(0 ,4));
 }
--- sandboxGallery.js
+++ sandboxGallery.js
@@ -2,9 +2,6 @@
 "use strict";
 console.log('SCRIPT: Creating and loading Sandbox Gallery  JS library')
 
-// photo = {
-//     photoName: {photoPath, caption, time}
-// }
 function GalleryGenerator(template, caption, date) {
     this.photos = []
     this.template = template
@@ -16,36 +13,58 @@ GalleryGenerator.prototype = {
 
     makeGallery: function(photoList) {
         const gallery = document.createElement('div');
+        const photos_container = document.createElement('div');
+        photos_container.id = "photos_container";
         gallery.id = 'sandboxgallery';
+        gallery.style.cssText = 'width: 100%; height: 100%; margin: auto;'
+
         // gallery.style.cssText = 'width: 100%; display: flex; justify-content: space-between; flex-wrap: wrap; margin-left: 1.5%;';
         // gallery.style.cssText = 'width: 800px; display: flex; justify-content: space-between; flex-wrap: wrap; margin-left: auto; margin-right: auto;';
 
         const body = $('body');
 		body.append(gallery);
+        gallery.append(photos_container);
         this.setImage(photoList);
         if (this.caption) {
             this.showCaption();
         }
+
         if (this.date) {
             this.showDate();
         }
-        this.setTemplate(this.template);
+        console.log(this.template);
+        if (this.template === 'mosaic') {
+            this.templateMosaic();
+        }
+        else if (this.template === 'grid') {
+            this.templateGrid();
+        }
+        else if (this.template === 'monsonry') {
+            this.templateMansonry();
+        }
+        else {
+            this.templateDefault();
+        }
     },
 
     setImage: function(photoList) {
         this.photos = photoList;
         const gallery = document.getElementById('sandboxgallery');
-        gallery.querySelectorAll('*').forEach(n => n.remove());
+        const photos_container = gallery.children[0];
+        photos_container.querySelectorAll('*').forEach(n => n.remove());
         this.photos.map(photo => {
             const div = document.createElement('div');
+            const a_tag = document.createElement('a');
             const img = document.createElement('img');
+            a_tag.href = photo.path;
             div.id = photo.name;
             img.id = photo.name + '_img';
             img.src = photo.path;
             div.style.cssText = 'width: 350px; height: 350px'
             img.style.cssText = 'width: 350px';
-            gallery.appendChild(div);
-            div.append(img);
+            photos_container.appendChild(div);
+            a_tag.append(img);
+            div.append(a_tag);
         });
     },
 
@@ -86,19 +105,7 @@ GalleryGenerator.prototype = {
 
     // parameter = templateName
     setTemplate: function(templateName) {
-        this.templates = templateName;
-        if (templateName === 'mosaic') {
-            this.templateMosaic();
-        }
-        else if (templateName === 'grid') {
-            this.templateGrid();
-        }
-        else if (templateName === 'monsonry') {
-            this.templateMansonry();
-        }
-        else {
-            this.templateDefault();
-        }
+        this.template = templateName;
     },
 
     showTemplate: function() {
@@ -110,18 +117,19 @@ GalleryGenerator.prototype = {
 
     templateDefault: function() {
         const gallery = document.getElementById('sandboxgallery');
-        gallery.style.cssText = 'width: 800px; display: flex; justify-content: space-between; flex-wrap: wrap; margin-left: auto; margin-right: auto;';
+        const photos_container = gallery.children[0];
+        photos_container.style.cssText = 'width: 100%; height: 500px; display: flex; justify-content: center; align-items: center';
 
         this.photos.map(photo => {
             const div = document.getElementById(photo.name);
             const img = document.getElementById(photo.name + '_img');
             const caption = document.getElementById(photo.name + '_caption');
             const date = document.getElementById(photo.name + '_date');
-            // if (div) {
-            //     div.style.cssText = 'background-color: pink;'
-            // }
+            if (div) {
+                div.style.cssText = 'width: 200px; height: 200px;'
+            }
             if (img) {
-                img.style.cssText = 'width: 350px; border-radius: 10%;'
+                img.style.cssText = 'width: 200px; height: 200px'
             }
             if (caption) {
                 caption.style.cssText = 'display: block; width: 100%; color: gray; letter-spacing: 1px; text-align: right; font-size: 15px; font-weight: 400;';
@@ -135,11 +143,34 @@ GalleryGenerator.prototype = {
 
     templateMosaic: function() {
         const gallery = document.getElementById('sandboxgallery');
-        gallery.style.cssText = 'width: 100%; display: flex; justify-content: space-between; flex-wrap: wrap;';
+        const photos_container = gallery.children[0];
+        photos_container.style.cssText = 'width: 100%; display: flex; flex-wrap: wrap;';
         console.log('template mosaic');
     },
 
     templateGrid: function() {
+        const gallery = document.getElementById('sandboxgallery');
+        const photos_container = gallery.children[0];
+        photos_container.style.cssText = 'width: 800px; display: flex; justify-content: space-between; flex-wrap: wrap; margin-left: auto; margin-right: auto;';
+
+        this.photos.map(photo => {
+            const div = document.getElementById(photo.name);
+            const img = document.getElementById(photo.name + '_img');
+            const caption = document.getElementById(photo.name + '_caption');
+            const date = document.getElementById(photo.name + '_date');
+            if (div) {
+                div.style.cssText = 'width: 350px; height: 350px;'
+            }
+            if (img) {
+                img.style.cssText = 'width: 350px; border-radius: 10%;'
+            }
+            if (caption) {
+                caption.style.cssText = 'display: block; width: 100%; color: gray; letter-spacing: 1px; text-align: right; font-size: 15px; font-weight: 400;';
+            }
+            if (date) {
+                date.style.cssText = 'display: block; width: 100%; color: darkgray; letter-spacing: 1px; text-align: right; font-size: 10px; font-weight: 200;';
+            }      
+        })
         console.log('template grid');
     },
 
